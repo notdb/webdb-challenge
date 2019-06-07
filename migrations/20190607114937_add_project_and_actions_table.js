5f780c4a44a715e932a70d49c61627f41b8b4cd1
Doug <me@douglasbowen.com>
2019-06-07 12:00:55 -0400

    add project table

--- migrations/20190607114937_add_project_and_actions_table.js
+++ migrations/20190607114937_add_project_and_actions_table.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("project", function(table) {
+    table.increments();
+
+    table.string("name", 128).notNullable();
+    table.string("description", 255).notNullable();
+    table.boolean("flag");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("project");
 };
